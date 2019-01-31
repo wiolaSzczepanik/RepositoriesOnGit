7154413af82523049319cd12e3aa887b794c033c
wiolaSzczepanik <wiolaszcz@gmail.com>
2019-01-31 11:56:59 +0100

    add files from Spring Boot Initializr

---
+++ src/main/java/com/gitrepositories/gitrepositories/GitrepositoriesApplication.java
@@ -0,0 +1,14 @@
+package com.gitrepositories.gitrepositories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitrepositoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitrepositoriesApplication.class, args);
+	}
+
+}
+
