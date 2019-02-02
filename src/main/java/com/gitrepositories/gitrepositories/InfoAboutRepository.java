3faa6064eb4a34078b0384c6bb566e431936a697
wiolaSzczepanik <wiolaszcz@gmail.com>
2019-02-02 20:53:54 +0100

    create getters and setters

--- src/main/java/com/gitrepositories/gitrepositories/InfoAboutRepository.java
+++ src/main/java/com/gitrepositories/gitrepositories/InfoAboutRepository.java
@@ -1,4 +1,23 @@
 package com.gitrepositories.gitrepositories;
 
 public class InfoAboutRepository {
+
+    String userName;
+    String repoName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getRepoName() {
+        return repoName;
+    }
+
+    public void setRepoName(String repoName) {
+        this.repoName = repoName;
+    }
 }
