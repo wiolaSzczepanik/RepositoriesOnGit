031d16a467d8a6f99b302643e5aabe495955df35
wiolaSzczepanik <wiolaszcz@gmail.com>
2019-02-02 20:57:05 +0100

    create rest Controller

--- src/main/java/com/gitrepositories/gitrepositories/RepositoryController.java
+++ src/main/java/com/gitrepositories/gitrepositories/RepositoryController.java
@@ -1,4 +1,18 @@
 package com.gitrepositories.gitrepositories;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RepositoryController {
+
+    @RequestMapping("epositories/{userName}/{repoName}")
+    InfoAboutRepository getUserAndRepoName(@PathVariable("userName") String userName,
+                                           @PathVariable("repoName") String repoName){
+        InfoAboutRepository repo = new InfoAboutRepository();
+        repo.setUserName(userName);
+        repo.setRepoName(repoName);
+        return repo;
+    }
 }
